a873c6d2e71e3f725454a40c27058b805ee0bf53
Aleš "programátor" Honsa <76952749+manicka1@users.noreply.github.com>
2022-02-27 23:29:28 +0100

    Add files via upload

---
+++ # využití knihovny math.py
@@ -0,0 +1,25 @@
+# využití knihovny math
+import math
+# import math přidá knhovnu math do programu
+# s knihovnou math se nám odemikají matematické funkce
+print(math.sin(1))
+# vypočítá goniometrickou funkci sinus daného čísla
+print(math.cos (1))
+# vypočítá goniometrickou funkci cosinus daného čísla
+print(math.tan(1))
+# výpočíta goniometrickou funkci tangents
+print(math.asin(1))
+# vypočítá hyperbolickou funkci tangents
+print(math.acos(1))
+# vypočítá hyperbolickou funkci tangents
+print(math.atan(1))
+# vypočítá hyperbolickou funkci tangents
+print(math.sqrt(75))
+#vypočítá druhou odmocniu z daného čísla
+print(math.pow(75,2))
+# umocní x na y
+print(math.pow(70,2)+20*7*5+pow(5,2))
+#pow(75,2)=pow(70,2)+20*7*5+pow(5,2) (tohle se dá použít u celočíselných mocnin od 1 do 99 se záměnou 7 a 5 na jiná čísla)
+print(math.log(10))
+#vypočítá přirozený logaritmus daného čísla
+print(math.log10(15))
